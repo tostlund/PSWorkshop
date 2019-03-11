9c64e1fabab4084677f642a164d7d888468eee57
tostlund <git@ring0.se>
2019-03-11 13:11:24 +0100

    agdjdgksdgj

--- thom/thom.ps1
+++ thom/thom.ps1
@@ -3,6 +3,17 @@
 #Get-Command
 $var = Get-ChildItem
 write-host $var
+Get-ChildItem
+if ($var) { write-host var assigned }
+else { write-host no var assigned}
+foreach ($test in $tests) {
+    write-host test
+}
 #read-host "test"
+# 1
+# 2
+# 3
+# 4
+
 
 #test comment
